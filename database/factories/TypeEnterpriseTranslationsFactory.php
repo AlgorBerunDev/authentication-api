206760e4e3f9fd99ba36b787c484645e2857c893
Doston <algorberun@gmail.com>
2021-07-18 21:18:04 +0500

    Created Seeders and Factory

---
+++ database/factories/TypeEnterpriseTranslationsFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TypeEnterpriseTranslations;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TypeEnterpriseTranslationsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TypeEnterpriseTranslations::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/TypesEmploymentOptionsFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TypesEmploymentOptions;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TypesEmploymentOptionsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TypesEmploymentOptions::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}

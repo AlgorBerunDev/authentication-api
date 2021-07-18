98cca0a140a9f48d571966aac7ec7378bd392eb0
Doston <algorberun@gmail.com>
2021-07-18 14:39:46 +0500

    User and Session table schema created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,29 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('identity')->unique();
+            $table->string('identitytype', 10)->default('not defined');
+            $table->string('username');
+            $table->timestamp('verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_blocked')->default(false);
+
+            // Ortiqcha verify codeni yubormaslik uchun yuborilgan verify codelar sonini xisoblash uchun kerak
+            // Agar kerakli limitdan oshib ketsa login_is_blocked_to kerakli vaqtgacha bloklanadi
+            $table->integer('verify_code_sended_count')->default(0);
+            $table->timestamps('verify_code_sended_at')->nullable();
+
+            // userga ortiqcha devicelar ulanishini oldini olish uchun sessiyalar soni biriktirilinadi userning o'zidan
+            // ammo super_session_max_count ham mavjud u tizim administratori orqali biriktiriliniladi
+            $table->integer('session_max_count')->default(10); // min 1
+            $table->integer('super_session_max_count')->default(100);
+
+            // agar aynan shu userga logining qilish kerakli limitdan oshsa, belgilangan vaqtgacha bloklab qo'yiladi
+            // (faqat logining uchun)
+            $table->timestamps('login_is_blocked_to')->nullable();
+            // xar safar login_is_blocked_to qiymatini berishda u qancha vaqt oralig'ida blokga tushganligiga qarab yana qancha vaqtga bloklash
+            // kerak bolgan vaqtni xisoblash uchun kerak
+            $table->integer('login_blocked_count')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2021_07_16_211009_create_sessions_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSessionsTable extends Migration
+{
+    /**
+     * Bu sessions xar bir device uchun bitta bo'ladi va userga biriktirilinadi.
+     * 2chi va undan ortiq devicelar tizimga kirib bo'lgan devicelarga yuborilgan verify_code orqali tizimga kirishi mumkin
+     * aks holda identity(telefon raqam yoki email manzil) ma'lumotiga asoslangan verify_code kerakli manzilga yuboriliniladi
+     * verify_code tasdiqlanmaguncha sessiya aktiv xolatni qabul qilmaydi va user nomidan amallar bajarish xuquqiga ega bo'lmaydi
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            /**
+             * secretKey va refreshKeyni qo'shilganliligini sababi kerakli klyuchlar hacker tomonidan aniqlangan taqdirda ham
+             * faqatgin bitta user ma'lumotiga dostupni beradi
+             * Agar bitta userning ma'lumotini ham xavfsizligini oshirmoqchi bo'lsak unda token yuboruvchining device va
+             * location ma'lumotlarini tekshirishimiz kerak, ammo bunga xojat yo'q sababi accessTokenning yashash davrini kamaytirsak ya'ni xakker kerakli
+             * kalitni topish uchun tokenni brut qilish uchun ketgan vaqtdan kamroq vaqt qo'yilsa unda xakker eski kalitga ega bo'ladi va u keraksiz bo'lib qoladi.
+             * ya'ni ungacha yangi secretKey va refreshKeylar xosil qilinib bo'liniladi.
+             *
+             */
+            $table->string('secretKey', 512);
+            $table->string('refreshKey', 512);
+            $table->text('refreshToken')->nullable();
+            $table->text('location')->nullable();
+            $table->text('device')->nullable();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('verify_code', 6);
+            // sessiya egasi tizimga kirish uchun created_at vaqtidan beri nechi marta verifikatsiya kodini tekshirib korganligining qiymati
+            // agar kerakli limitdan oshib ketgan bo'lsa unda sessiya o'chiriliniladi va user qaytadan login qilib sessiya olishiga to'g'ri keladi
+            $table->integer('verify_count')->default(0);
+            $table->timestamps('verified_at');
+
+            $table->boolean('is_activated')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+}

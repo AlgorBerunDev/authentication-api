2b040b554476ca09ccb89a2e5d2886bcf740263f
Doston <algorberun@gmail.com>
2021-07-18 17:11:12 +0500

    Created Account schema and all relations

---
+++ database/migrations/2021_07_18_111011_create_type_enterprise_and_accounts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTypeEnterpriseAndAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('type_enterprise_and_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('account_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('enterprise_option_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('type_enterprise_and_accounts');
+    }
+}

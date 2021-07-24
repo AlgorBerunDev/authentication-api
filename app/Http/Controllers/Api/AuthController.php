16fcedcf7b2e487cfa37f7df1bfc921f05460fa6
Doston <algorberun@gmail.com>
2021-07-24 07:42:14 +0500

    User basic registration completed

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Exceptions\TestException;
+use App\Exceptions\ValidatorException;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -24,18 +26,54 @@ class AuthController extends Controller
         ]);
 
         if($validator->fails()){
-
-            return response()->json($validator->messages(), 400);
+            throw new ValidatorException($validator->messages());
         }
 
         $user = User::create(array_merge(
-                    $validator->validated(),
-                    ['password' => bcrypt($request->password)],
-                ));
+            $validator->validated(),
+            ['password' => bcrypt($request->password)],
+        ));
 
         return response()->json([
-            'message' => 'User successfully registered',
+            'description' => 'User successfully registered',
             'user' => $user
         ], 201);
+    }
+
+    public function test(Request $request){
+        // 'identity' => 'savion11@example.org',
+        // 'password' => 'password1'
+
+
+        throw new TestException(['message' => 'test']);
+
+        $validator = Validator::make($request->all(), [
+            'identity' => 'required|string|max:100',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if($validator->fails()){
+            return response()->json(
+                array_merge([
+                    'errors' => $validator->messages(),
+                    'message' => 'Validation error',
+                    'statusCode' => 0
+                ])
+                , 400);
+        }
+
+        try {
+            $users = User::attempt($request->all());
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'identity or password not condition',
+                'errors' => [],
+                'statusCode' => 0
+            ], 403);
+        }
+
+        return response()->json([
+            'users' => $users
+        ]);
     }
 }

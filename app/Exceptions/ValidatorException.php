fa5c75b62de9dff2ef25d591695b115986e9932c
Doston <algorberun@gmail.com>
2021-07-24 07:20:09 +0500

    Created ValidatorExceptoin

---
+++ app/Exceptions/ValidatorException.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\MessageBag;
+
+class ValidatorException extends Exception
+{
+    private $messages;
+    public function __construct(MessageBag $messages){
+        // dd($messages);
+        $this->messages = $messages;
+    }
+    public function render() {
+
+        $errors = $this->messages->toArray();
+
+        $result = [];
+        foreach ($errors as $key => $value) {
+            array_push($result, [
+                'field' => $key,
+                'messages' => $value
+            ]);
+        }
+        return response()->json(
+            array_merge([
+                'errors' => $result,
+                'messages' => 'Validation error',
+                'statusCode' => 0
+            ])
+            , 400);
+    }
+}

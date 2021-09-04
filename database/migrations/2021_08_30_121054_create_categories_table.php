b8007b1aa2176b7fa33d0e43ed50477b93557dbb
Doston <algorberun@gmail.com>
2021-09-04 21:37:32 +0500

    Categories migrations changed

---
+++ database/migrations/2021_08_30_121054_create_categories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string('name');
+            $table->string('url');
+            $table->string('name_format');
+            $table->boolean('published')->default(false);
+            $table->timestamp('published_at')->useCurrent();
+            $table->string('icon')->nullable();
+            $table->string('background_image')->nullable();
+            $table->string('avatar_image')->nullable();
+            $table->timestamps();
+            $table->foreign('parent_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
---
+++ database/migrations/2021_08_30_133040_create_category_translations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_translations', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->text('description');
+            $table->string('info_url');
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->unsignedBigInteger('language_id');
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_translations');
+    }
+}

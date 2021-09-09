f1c1791c7f18929820e1bcf84e7896627bb5febe
Doston <algorberun@gmail.com>
2021-09-09 18:32:21 +0500

    Added category routes

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\SessionController;
-
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\CategoryTranslationController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -26,7 +27,6 @@ Route::group([
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/send_confirmation', [AuthController::class, 'sendConfirmation'])->middleware('apiauth');
     Route::post('/confirmation', [AuthController::class, 'confirmation'])->middleware('apiauth');
-    // Route::get('/user-profile', [AuthController::class, 'userProfile']);
 });
 Route::group([
     'middleware' => 'api',
@@ -45,4 +45,28 @@ Route::group([
     Route::post('/', [SessionController::class, 'getSessions']);
     Route::delete('/', [SessionController::class, 'removeSessions']);
     Route::patch('/', [SessionController::class, 'updateFcmToken']);
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'categories'
+], function ($router) {
+    Route::get('/', [CategoryController::class, 'getAll']);
+    Route::get('/base_parents', [CategoryController::class, 'getSuperParent']);
+    Route::get('/{id}', [CategoryController::class, 'getById']);
+    Route::get('/childs/{parent_id}', [CategoryController::class, 'getChilds']);
+    Route::post('/', [CategoryController::class, 'create']);
+    Route::post('/{id}', [CategoryController::class, 'update']);
+    Route::delete('/{id}', [CategoryController::class, 'remove']);
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'category_translations'
+], function ($router) {
+    Route::get('/', [CategoryTranslationController::class, 'getAll']);
+    Route::get('/{parent_id}', [CategoryTranslationController::class, 'getByCategoryId']);
+    Route::post('/', [CategoryTranslationController::class, 'create']);
+    Route::patch('/{id}', [CategoryTranslationController::class, 'update']);
+    Route::delete('/{id}', [CategoryTranslationController::class, 'remove']);
 });
